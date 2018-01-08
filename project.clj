d7996c47038faf0039064e5a7ee57c7f35072afa
Albert Lai <albert.lai@algotizer.com>
2018-01-08 21:52:15 +0800

    make dev profile works with npm-deps

--- project.clj
+++ project.clj
@@ -1,12 +1,16 @@
 (defproject test-npm-deps "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
-                 [reagent "0.8.0-alpha2"]
+                 [reagent "0.8.0-alpha2" :exclusions [cljsjs/react
+                                                      cljsjs/react-dom
+                                                      cljsjs/create-react-class]]
                  [re-frame "0.10.2"]]
 
   :plugins [[lein-cljsbuild "1.1.5"]]
 
   :min-lein-version "2.5.3"
+
+  :jvm-opts ["--add-modules=java.xml.bind"]
 
   :source-paths ["src/clj"]
 
@@ -29,6 +33,10 @@
                     :output-to            "resources/public/js/compiled/app.js"
                     :output-dir           "resources/public/js/compiled/out"
                     :asset-path           "js/compiled/out"
+                    :install-deps true
+                    :npm-deps {:react "16.2.0"
+                               :react-dom "16.2.0"
+                               :create-react-class "15.6.2"}
                     :source-map-timestamp true
                     :preloads             [devtools.preload]
                     :external-config      {:devtools/config {:features-to-install :all}}
@@ -39,6 +47,7 @@
      :compiler     {:main            test-npm-deps.core
                     :output-to       "resources/public/js/compiled/app.js"
                     :optimizations   :advanced
+                    :process-shim true
                     :closure-defines {goog.DEBUG false}
                     :pretty-print    false}}
 
